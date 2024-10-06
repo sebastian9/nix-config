60f4fede29cf6a78d30ad68ce40762f638c83ac1
sebastian9 <sebastianls0228@gmail.com>
2024-10-06 14:32:00 -0700

    Minimal photoprism installation

--- hosts/nuc/services/photoprism.nix
+++ hosts/nuc/services/photoprism.nix
@@ -1,1 +1,27 @@
-{}
+{ pkgs, ...}:
+{
+
+  environment.systemPackages = [ pkgs.photoprism ];
+  environment.etc."photoprism-admin-pass".text = "test_phrase";
+  networking.firewall.allowedTCPPorts = [ 2342 ];
+
+  services.photoprism = {
+    enable = true;
+    passwordFile = "/etc/photoprism-admin-pass";
+    port = 2342;
+    originalsPath = "/mnt/synology/seb/photoprism/originals";
+    importPath = "/mnt/synology/seb/photoprism/imports";
+    address = "0.0.0.0";
+    settings = {
+      PHOTOPRISM_ADMIN_USER = "admin";
+      PHOTOPRISM_ADMIN_PASSWORD = "...";
+      PHOTOPRISM_DEFAULT_LOCALE = "en";
+      PHOTOPRISM_DATABASE_DRIVER = "mysql";
+      PHOTOPRISM_DATABASE_NAME = "photoprism";
+      PHOTOPRISM_DATABASE_SERVER = "/run/mysqld/mysqld.sock";
+      PHOTOPRISM_DATABASE_USER = "photoprism";
+      PHOTOPRISM_SITE_URL = "http://nuc.home:2342";
+      PHOTOPRISM_SITE_TITLE = "Seb's PhotoPrism";
+    };
+  };
+}
