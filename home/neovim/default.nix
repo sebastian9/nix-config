f88687ad09f0b13f9b424479e81ffe6570bfe74c
sebastian9 <sebastianls0228@gmail.com>
2024-10-15 08:09:28 -0700

    Neovim with treesitter :)

--- home/neovim/default.nix
+++ home/neovim/default.nix
@@ -1,9 +1,84 @@
 { pkgs, ... }:
 {
   programs.nixvim = {
+
     enable = true;
 
-    colorschemes.catppuccin.enable = true;
-    plugins.lualine.enable = true;
+    opts = {
+      mouse = "a";
+      relativenumber = true;
+      number = true;
+      ignorecase = true;
+    };
+
+    globals.mapleader = " ";
+    keymaps = [
+      {
+        key = "<leader><Tab>";
+        action = ":b#<CR>";
+      }
+      {
+        key = "<leader>n";
+        action = ":Ex<CR>";
+      }
+    ];
+
+
+    colorscheme = "midnight";
+
+    extraPlugins = with pkgs.vimPlugins; [
+      midnight-nvim # color theme
+      vim-lastplace
+      YouCompleteMe
+    ];
+
+    plugins = {
+
+      nix.enable = true;
+
+      lualine.enable = true;
+
+      treesitter = {
+        enable = true;
+      };
+
+      telescope = {
+
+        enable = true;
+
+        keymaps = {
+          "<leader>/" = "live_grep";
+          "<C-p>" = "git_files"; # respects git_ignore
+          "<leader>ff" = "find_files";
+          "<leader>fb" = "buffers";
+          "<leader>fc" = "commands";
+          "<leader>fk" = "keymaps";
+          "<leader>fm" = "marks";
+          "<leader>fh" = "help_tags";
+        };
+
+        defaults = {
+          file_ignore_patterns = [
+            "^.git/"
+            "^.mypy_cache/"
+            "^__pycache__/"
+            "^output/"
+            "^data/"
+            "%.ipynb"
+          ];
+          set_env.colorterm = "truecolor";
+          selection_caret = "> ";
+          mappings = {
+            i = {
+              "<C-j>".__raw = "require('telescope.actions').move_selection_next";
+              "<C-k>".__raw = "require('telescope.actions').move_selection_previous";
+            };
+          };
+        };
+
+      };
+
+    };
+
   };
 }
--- home/vim.nix
+++ home/vim.nix
@@ -24,11 +24,11 @@
       set mouse=a
       let mapleader=" "
       let $FZF_DEFAULT_COMMAND = 'fd --type f --hidden --follow --exclude .git --exclude node_modules'
-      map <leader>f :Files<CR>
-      map <leader>b :Buffers<CR>
-      map <leader>c :Commands<CR>
-      map <leader>m :Marks<CR>
-      map <leader>h :Helptags<CR>
+      map <leader>ff :Files<CR>
+      map <leader>fb :Buffers<CR>
+      map <leader>fc :Commands<CR>
+      map <leader>fm :Marks<CR>
+      map <leader>fh :Helptags<CR>
       map <leader>/ :Rg<CR>
       map <leader><Tab> :b#<CR>
       map <leader>e :Ex<CR>
