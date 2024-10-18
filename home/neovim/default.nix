68194c2a65d0eac6b80ce0be85e8897db67883c1
sebastian9 <sebastianls0228@gmail.com>
2024-10-18 15:05:41 -0700

    Document settings

--- home/neovim/default.nix
+++ home/neovim/default.nix
@@ -21,25 +21,60 @@
 
     opts = {
       # You can search for any of these using Telescope help_tags
+
+      # Enable mouse
       mouse = "a";
-      relativenumber = true;
+
+      # Wrap lines
+      wrap = true;
+
+      # Line numbers
       number = true;
-      ignorecase = true;
-      smartcase = true; # Don't ignore case with capitals
-      scrolloff = 8;
-      hlsearch = true;
-      incsearch = true;
+      relativenumber = true;
+
+      # Tabs and identation
       autoindent = true;
       smartindent = true;
       expandtab = true;
       tabstop = 4;
       softtabstop = 4;
+
+      # Minimum number of lines visible when scrolling up/down
+      scrolloff = 8;
+
+      # Enable ignorecase + smartcase for better searching
+      ignorecase = true;
+      smartcase = true; # Don't ignore case with capitals
+
+      # Set encoding type
+      encoding = "utf-8";
+      fileencoding = "utf-8";
+
+      # Persist undo history
       undofile = true;
-      wrap = true;
       swapfile = false;
       backup = false;
+
+      # Search behavior
+      hlsearch = true;
+      incsearch = true;
+
+      # shows the effects of |:substitute|, |:smagic|,
+      # |:snomagic| and user commands with the |:command-preview|
+      # flag as you type.
+      inccommand = "split";
+
+      # We don't need to see things like INSERT anymore
+      showmode = false;
+
+      # Faster update time, default 4000
       updatetime = 50;
-      virtualedit = "block"; # nice visual-block
+
+      # nice visual-block
+      virtualedit = "block";
+
+      # Enable the sign column to prevent the screen from jumping
+      signcolumn = "yes";
     };
 
     plugins = {
