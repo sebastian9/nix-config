3f73055b679a50234ecc019a8ddaa02b173156a6
sebastian9 <sebastianls0228@gmail.com>
2024-10-18 14:47:46 -0700

    Add telescope extensions

--- home/neovim/plugins/telescope.nix
+++ home/neovim/plugins/telescope.nix
@@ -2,6 +2,25 @@
   programs.nixvim.plugins = {
     telescope = {
       enable = true;
+      extensions = {
+        fzf-native = {
+          enable = true;
+          settings = {
+            fuzzy = true;
+            override_generic_sorter = true;
+            override_file_sorter = true;
+            case_mode = "smart_case";
+          };
+        };
+        ui-select = {
+          enable = true;
+          settings = {
+            specific_opts = {
+              codeactions = true;
+            };
+          };
+        };
+      };
       settings.defaults = {
         file_ignore_patterns = [
           "^.git/"
