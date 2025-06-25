9c5f20f259dc762af0d18da2bf76bf961877f610
slopezsanchez <slopezsanchez@tesla.com>
2025-06-25 16:26:15 -0700

    Omnisharp

--- home/neovim/standalone/plugins/language_specific/dotnet.nix
+++ home/neovim/standalone/plugins/language_specific/dotnet.nix
@@ -1,8 +1,22 @@
 _: {
   programs.nixvim.plugins = {
+    # none-ls.sources = {
+    #   formatting.csharpier.enable = true;
+    # };
     lsp = {
       servers = {
         csharp_ls.enable = true;
+        omnisharp = {
+          enable = true;
+
+          settings = {
+            enableRoslynAnalyzers = true;
+            enableEditorConfigSupport = true;
+            enableImportCompletion = true;
+            sdkIncludePrereleases = true;
+            analyzeOpenDocumentsOnly = true;
+          };
+        };
       };
     };
   };
